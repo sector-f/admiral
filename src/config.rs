ebba2ba126ebfd257b9bb326e0fe577022fdfb6a
expectocode <expectocode@gmail.com>
2019-10-15 01:35:07 +0100

    Apply rustfmt.

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,12 @@
 extern crate serde;
-use config::serde::de::{Deserialize, Deserializer};
 use config::serde::de::Error;
+use config::serde::de::{Deserialize, Deserializer};
 
-use toml::Value;
 use toml::value::Table;
+use toml::Value;
 
-use std::path::PathBuf;
 use std::env;
+use std::path::PathBuf;
 
 #[derive(Debug, Serialize)]
 pub struct ConfigFile {
@@ -14,46 +14,35 @@ pub struct ConfigFile {
 }
 
 impl<'de> Deserialize<'de> for ConfigFile {
-    fn deserialize<D>(d: D) -> Result<ConfigFile, D::Error> where D: Deserializer<'de> {
+    fn deserialize<D>(d: D) -> Result<ConfigFile, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
         let value = Value::deserialize(d)?;
         match Value::try_into::<Table>(value) {
-            Ok(mut table) => {
-                match table.remove("admiral") {
-                    Some(admiral) => {
-                        match Value::try_into::<ItemList>(admiral) {
-                            Ok(admiral) => {
-                                let mut scripts = Vec::new();
-                                for item in admiral.items {
-                                    if let Some(v) = table.get(&item) {
-                                        match Value::try_into::<Script>(v.to_owned()) {
-                                            Ok(s) => {
-                                                scripts.push(s);
-                                            },
-                                            Err(e) => {
-                                                return Err(Error::custom(e.to_string()));
-                                            },
-                                        }
+            Ok(mut table) => match table.remove("admiral") {
+                Some(admiral) => match Value::try_into::<ItemList>(admiral) {
+                    Ok(admiral) => {
+                        let mut scripts = Vec::new();
+                        for item in admiral.items {
+                            if let Some(v) = table.get(&item) {
+                                match Value::try_into::<Script>(v.to_owned()) {
+                                    Ok(s) => {
+                                        scripts.push(s);
+                                    }
+                                    Err(e) => {
+                                        return Err(Error::custom(e.to_string()));
                                     }
                                 }
-                                Ok(
-                                    ConfigFile {
-                                        scripts: scripts,
-                                    }
-                                )
-                            },
-                            Err(e) => {
-                                Err(Error::custom(e.to_string()))
-                            },
+                            }
                         }
-                    },
-                    None => {
-                        Err(Error::custom("missing \"admiral\" section".to_string()))
-                    },
-                }
+                        Ok(ConfigFile { scripts: scripts })
+                    }
+                    Err(e) => Err(Error::custom(e.to_string())),
+                },
+                None => Err(Error::custom("missing \"admiral\" section".to_string())),
             },
-            Err(e) => {
-                Err(Error::custom(e.to_string()))
-            },
+            Err(e) => Err(Error::custom(e.to_string())),
         }
     }
 }
@@ -76,32 +65,39 @@ impl Script {
     pub fn shell(&self) -> PathBuf {
         match self.shell {
             Some(ref shell) => PathBuf::from(&shell),
-            None => {
-                match env::var_os("SHELL") {
-                    Some(sh) => {
-                        PathBuf::from(&sh)
-                    },
-                    None => {
-                        PathBuf::from("/bin/sh")
-                    }
-                }
-            }
+            None => match env::var_os("SHELL") {
+                Some(sh) => PathBuf::from(&sh),
+                None => PathBuf::from("/bin/sh"),
+            },
         }
     }
 }
 
 #[allow(dead_code)]
 pub fn get_config_file() -> Option<PathBuf> {
-    let xdg_path = env::var("XDG_CONFIG_HOME").ok()
+    let xdg_path = env::var("XDG_CONFIG_HOME")
+        .ok()
         .map(|v| PathBuf::from(v).join("admiral.d").join("admiral.toml"))
         .and_then(if_readable);
 
-    let dot_home = env::var("HOME").ok()
-        .map(|v| PathBuf::from(v).join(".config").join("admiral.d").join("admiral.toml"))
+    let dot_home = env::var("HOME")
+        .ok()
+        .map(|v| {
+            PathBuf::from(v)
+                .join(".config")
+                .join("admiral.d")
+                .join("admiral.toml")
+        })
         .and_then(if_readable);
 
     xdg_path.or(dot_home)
 }
 
 #[allow(dead_code)]
-fn if_readable(path: PathBuf) -> Option<PathBuf> { if path.exists() { Some(path) } else { None } }
+fn if_readable(path: PathBuf) -> Option<PathBuf> {
+    if path.exists() {
+        Some(path)
+    } else {
+        None
+    }
+}
